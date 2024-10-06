1ae7078679ccd4d11c6490a47f22839f93d3ae2e
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-10-06 22:22:45 +0900

    add: responsive style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,42 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className={clsx("px-5 pb-20 pt-48 text-center")}>
-          <h1 className={clsx("mb-5 text-7xl font-black")}>
+        <div
+          className={clsx(
+            "px-5 pb-20 pt-48 text-center -lg:pb-16 -lg:pt-40 -md:pt-32 -sm:pb-12 -sm:pt-24"
+          )}
+        >
+          <h1
+            className={clsx(
+              "mb-5 text-7xl font-black -lg:text-6xl -md:text-5xl -sm:text-4xl"
+            )}
+          >
             ご自由にお取りください.com
           </h1>
-          <p className={clsx("text-9xl")}>🤲</p>
+          <p
+            className={clsx("text-9xl -lg:text-8xl -md:text-7xl -sm:text-6xl")}
+          >
+            🤲
+          </p>
         </div>
         <div
-          className={clsx("flex flex-col gap-24 bg-gray-100 px-5 pb-52 pt-32")}
+          className={clsx(
+            "flex flex-col gap-24 bg-gray-100 px-5 pb-52 pt-32 -lg:pb-44 -lg:pt-28 -md:gap-16 -md:pb-40 -md:pt-24 -sm:gap-24 -sm:pb-28 -sm:pt-16"
+          )}
         >
           <section>
             <div
               className={clsx(
-                "flex flex-row-reverse items-center justify-center gap-5"
+                "flex flex-row-reverse items-center justify-center gap-5 -sm:flex-col -sm:gap-4"
               )}
             >
               <div>
-                <h2 className={clsx("mb-5 text-4xl")}>一問一答メーカー</h2>
-                <p className={clsx("max-w-md text-xl")}>
+                <h2 className={clsx("mb-5 text-4xl -lg:text-3xl -md:text-2xl")}>
+                  一問一答メーカー
+                </h2>
+                <p
+                  className={clsx("max-w-md text-xl -lg:text-lg -md:text-base")}
+                >
                   オリジナルの一問一答が作成できるアプリです。学習にお役立てください。
                 </p>
               </div>
@@ -36,7 +54,9 @@ export default function Home() {
                     src={"/question-and-answer.png"}
                     width={1200}
                     height={482}
-                    className={clsx("max-w-xl")}
+                    className={clsx(
+                      "max-w-xl -lg:max-w-md -md:max-w-sm -sm:max-w-sm"
+                    )}
                     alt="一問一答メーカー"
                   />
                 </Link>
@@ -46,12 +66,16 @@ export default function Home() {
           <section>
             <div
               className={clsx(
-                "flex flex-row-reverse items-center justify-center gap-5"
+                "flex flex-row-reverse items-center justify-center gap-5 -sm:flex-col -sm:gap-4"
               )}
             >
               <div>
-                <h2 className={clsx("mb-5 text-4xl")}>ステメモ</h2>
-                <p className={clsx("max-w-md text-xl")}>
+                <h2 className={clsx("mb-5 text-4xl -lg:text-3xl -md:text-2xl")}>
+                  ステメモ
+                </h2>
+                <p
+                  className={clsx("max-w-md text-xl -lg:text-lg -md:text-base")}
+                >
                   簡易メモアプリです。ちょっとした用事やお買い物のメモにどうぞ。
                 </p>
               </div>
@@ -64,7 +88,9 @@ export default function Home() {
                     src={"/sutememo.png"}
                     width={1200}
                     height={482}
-                    className={clsx("max-w-xl")}
+                    className={clsx(
+                      "max-w-xl -lg:max-w-md -md:max-w-sm -sm:max-w-sm"
+                    )}
                     alt="ステメモ"
                   />
                 </Link>
@@ -73,7 +99,7 @@ export default function Home() {
           </section>
         </div>
       </main>
-      <footer className={clsx("bg-gray-100 pb-7")}>
+      <footer className={clsx("bg-gray-100 pb-7 -md:pb-6 -sm:pb-3")}>
         <p className={clsx("text-center text-sm tracking-wide")}>
           <small>© 2024 ご自由にお取りください.com</small>
         </p>
