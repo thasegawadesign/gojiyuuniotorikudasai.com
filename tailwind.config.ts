61a0a8ae0e36bde6fa2fbd5230c27209ef18d9a6
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-10-06 21:30:09 +0900

    add: reverse media queries option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        "-2xl": { max: "1535px" },
+        "-xl": { max: "1279px" },
+        "-lg": { max: "1023px" },
+        "-md": { max: "767px" },
+        "-sm": { max: "639px" },
       },
     },
   },
